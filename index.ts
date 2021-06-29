aa9787b1e5246c6219f39785b238bcd70635751b
Tommaso Morganti <tommaso.morganti01@gmail.com>
2021-06-29 15:38:12 +0200

    fixed gitignore, whops

--- index.ts
+++ index.ts
@@ -96,13 +96,11 @@ let main = async () => {
         process.chdir(`..`)
     }
 
-    await call('git init')
-
     console.log(c.bgWhiteBright.black`all done! ❤️ `)
 
     let ans = (await ask('git init?')).toLowerCase()
     if (ans !== 'n' && ans !== 'no') {
-        await fs.move(ignorePath, path.join(folderName, '/.gitignore'))
+        await fs.move(ignorePath, '/.gitignore')
         await call('git init')
         await call('git add .')
         await call('git commit -m "initial commit"')
